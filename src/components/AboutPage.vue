fce6c7ea0db636af9752fa6b7c36b42743349dae
Chris Lucas <chris.lucas@geodan.nl>
2019-07-02 11:19:15 +0200

    Use vue transition to toggle about page

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,116 +1,118 @@
 <template>
-  <div class="about-page page-closed">
-    <div class="content">
-      <header>
-        <TheLogo
-          titleColor="#4B4B4B"
-          subtitleColor="#4B4B4B"
-          lineColor="#A8A8A8"
-        />
-        <NavigationButton title="Go back to the app" @click.native="close" />
-      </header>
-      <div>
-        <section class="about">
-          <h3>ABOUT THIS APPLICATION</h3>
-          <p>
-            Accessing Campscapes: Inclusive Strategies for Using European
-            Conflicted Heritage’ (iC-ACCESS) looks at traces of the 20th century
-            mass violence and terror, as tangible reminders of the “age of
-            extremes” and their present uses in (trans)national contexts. In
-            most post-war European countries former Nazi internment camps have
-            become icons of antifascist resistance and the Holocaust, and they
-            have played a consistent role in postwar European memory of
-            totalitarianism and genocide. In the Eastern European centre of the
-            Holocaust and Communist terror, many former ‘terrorscapes’ are still
-            contested spaces where consecutive internments of prisoners by
-            occupying powers and authoritarian regimes transformed the victims
-            of one event into the persecutors of another. This entanglement of
-            remembering with forgetting and the silencing of competing
-            narratives.
-          </p>
-        </section>
-        <section class="team">
-          <h3>TEAM</h3>
-          <ul>
-            <TeamMember
-              name="Rob van der Laarse"
-              position="Coordinator"
-              organization="Universiteit van Amsterdam"
-            />
-            <TeamMember
-              name="Dana Dolghin"
-              position="Content"
-              organization="Universiteit van Amsterdam"
-            />
-            <TeamMember
-              name="Tijm Lanjouw"
-              position="3D Modeller"
-              organization="4DLab"
-            />
-            <TeamMember
-              name="Maurice de Kleijn"
-              position="Coordinator"
-              organization="SPINLab"
-            />
-            <TeamMember
-              name="Jitte Waagen"
-              position="Project Lead"
-              organization="4DLab"
-            />
-            <TeamMember
-              name="Chris Lucas"
-              position="Front-end"
-              organization="SPINLab"
-            />
-            <TeamMember
-              name="Bas Kortholt"
-              position="Coordinator"
-              organization="Herinneringscentrum Westerbork"
-            />
-            <TeamMember
-              name="Erik van Zummeren"
-              position="UI-Design"
-              organization="."
-            />
-            <TeamMember
-              name="Ivan Kisjes"
-              position="Server Management"
-              organization="Create"
-            />
-          </ul>
-        </section>
-        <section class="partners">
-          <h3>PROJECT PARTNERS</h3>
-          <ul>
-            <li>
-              <img
-                :src="require('../assets/logo_partners/uva.png')"
-                alt="Universiteit van Amsterdam"
+  <transition name="fade">
+    <div v-show="isOpen" class="about-page">
+      <div class="content">
+        <header>
+          <TheLogo
+            titleColor="#4B4B4B"
+            subtitleColor="#4B4B4B"
+            lineColor="#A8A8A8"
+          />
+          <NavigationButton title="Go back to the app" @click.native="close" />
+        </header>
+        <div>
+          <section class="about">
+            <h3>ABOUT THIS APPLICATION</h3>
+            <p>
+              Accessing Campscapes: Inclusive Strategies for Using European
+              Conflicted Heritage’ (iC-ACCESS) looks at traces of the 20th
+              century mass violence and terror, as tangible reminders of the
+              “age of extremes” and their present uses in (trans)national
+              contexts. In most post-war European countries former Nazi
+              internment camps have become icons of antifascist resistance and
+              the Holocaust, and they have played a consistent role in postwar
+              European memory of totalitarianism and genocide. In the Eastern
+              European centre of the Holocaust and Communist terror, many former
+              ‘terrorscapes’ are still contested spaces where consecutive
+              internments of prisoners by occupying powers and authoritarian
+              regimes transformed the victims of one event into the persecutors
+              of another. This entanglement of remembering with forgetting and
+              the silencing of competing narratives.
+            </p>
+          </section>
+          <section class="team">
+            <h3>TEAM</h3>
+            <ul>
+              <TeamMember
+                name="Rob van der Laarse"
+                position="Coordinator"
+                organization="Universiteit van Amsterdam"
               />
-            </li>
-            <li>
-              <img
-                :src="require('../assets/logo_partners/kampwesterbork.png')"
-                alt="Herinneringscentrum Kamp Westerbork"
+              <TeamMember
+                name="Dana Dolghin"
+                position="Content"
+                organization="Universiteit van Amsterdam"
               />
-            </li>
-            <li>
-              <img
-                :src="require('../assets/logo_partners/hera.png')"
-                alt="HERA"
+              <TeamMember
+                name="Tijm Lanjouw"
+                position="3D Modeller"
+                organization="4DLab"
               />
-            </li>
-            <li>
-              <img
-                :src="require('../assets/logo_partners/rotary.png')"
-                alt="Rotary"
+              <TeamMember
+                name="Maurice de Kleijn"
+                position="Coordinator"
+                organization="SPINLab"
               />
-            </li>
-          </ul>
-        </section>
+              <TeamMember
+                name="Jitte Waagen"
+                position="Project Lead"
+                organization="4DLab"
+              />
+              <TeamMember
+                name="Chris Lucas"
+                position="Front-end"
+                organization="SPINLab"
+              />
+              <TeamMember
+                name="Bas Kortholt"
+                position="Coordinator"
+                organization="Herinneringscentrum Westerbork"
+              />
+              <TeamMember
+                name="Erik van Zummeren"
+                position="UI-Design"
+                organization="."
+              />
+              <TeamMember
+                name="Ivan Kisjes"
+                position="Server Management"
+                organization="Create"
+              />
+            </ul>
+          </section>
+          <section class="partners">
+            <h3>PROJECT PARTNERS</h3>
+            <ul>
+              <li>
+                <img
+                  :src="require('../assets/logo_partners/uva.png')"
+                  alt="Universiteit van Amsterdam"
+                />
+              </li>
+              <li>
+                <img
+                  :src="require('../assets/logo_partners/kampwesterbork.png')"
+                  alt="Herinneringscentrum Kamp Westerbork"
+                />
+              </li>
+              <li>
+                <img
+                  :src="require('../assets/logo_partners/hera.png')"
+                  alt="HERA"
+                />
+              </li>
+              <li>
+                <img
+                  :src="require('../assets/logo_partners/rotary.png')"
+                  alt="Rotary"
+                />
+              </li>
+            </ul>
+          </section>
+        </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -119,20 +121,23 @@ import NavigationButton from "./NavigationButton";
 import TeamMember from "./TeamMember";
 
 export default {
-  name: "AboutPagePage",
+  name: "AboutPage",
   components: {
     TheLogo,
     NavigationButton,
     TeamMember
   },
+  data() {
+    return {
+      isOpen: false
+    };
+  },
   methods: {
     close() {
-      this.$el.classList.remove("page-open");
-      this.$el.classList.add("page-closed");
+      this.isOpen = false;
     },
     open() {
-      this.$el.classList.remove("page-closed");
-      this.$el.classList.add("page-open");
+      this.isOpen = true;
     }
   }
 };
@@ -148,29 +153,14 @@ export default {
   overflow: auto;
   background-color: #ebebeb;
   color: #4a4a4a;
-  visibility: hidden;
   z-index: 9;
   display: flex;
   flex-direction: column;
   align-items: center;
 }
 
-.page-open {
-  visibility: visible;
-  opacity: 1;
-  transition: opacity 0.1s ease-out;
-}
-
-.page-closed {
-  visibility: hidden;
-  opacity: 0;
-  transition: visibility 0s 0.1s, opacity 0.1s ease-out;
-}
-
 .content {
   max-width: 97.625rem;
-  /* max-height: 100vh;
-  overflow: hidden; */
   padding: 1rem;
 }
 
