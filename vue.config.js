e8da8d7f6f111ff49bff2539cf2b14c2746b0161
Chris Lucas <chris.lucas@geodan.nl>
2019-10-16 12:57:05 +0200

    Only copy over necessary files from libs

--- vue.config.js
+++ vue.config.js
@@ -5,19 +5,55 @@ module.exports = {
   configureWebpack: {
     plugins: [
       new CopyWebpackPlugin([
-        { from: "./node_modules/@tweenjs/", to: "libs/@tweenjs/" }
+        {
+          from: "./node_modules/@tweenjs/tween.js/src/Tween.js",
+          to: "libs/tweenjs/Tween.js"
+        }
       ]),
       new CopyWebpackPlugin([
-        { from: "./node_modules/jquery/", to: "libs/jquery/" }
+        {
+          from: "./node_modules/jquery/dist/jquery.min.js",
+          to: "libs/jquery/jquery.min.js"
+        }
       ]),
       new CopyWebpackPlugin([
-        { from: "./node_modules/three/", to: "libs/three/" }
+        {
+          from: "./node_modules/three/build/three.min.js",
+          to: "libs/three/three.min.js"
+        }
       ]),
       new CopyWebpackPlugin([
-        { from: "./node_modules/proj4", to: "libs/proj4/" }
+        {
+          from: "./node_modules/proj4/dist/proj4.js",
+          to: "libs/proj4/proj4.js"
+        }
       ]),
       new CopyWebpackPlugin([
-        { from: "./node_modules/potree", to: "libs/potree/" }
+        {
+          from: "./node_modules/potree/build/potree/potree.js",
+          to: "libs/potree/potree.js"
+        }
+      ]),
+      new CopyWebpackPlugin([
+        {
+          from: "./node_modules/potree/build/potree/workers/",
+          to: "libs/potree/workers/"
+        }
+      ]),
+      new CopyWebpackPlugin([
+        { from: "./node_modules/potree/libs/", to: "libs/potree/libs/" }
+      ]),
+      new CopyWebpackPlugin([
+        {
+          from: "./node_modules/potree/resources/icons/",
+          to: "libs/potree/resources/icons/"
+        }
+      ]),
+      new CopyWebpackPlugin([
+        {
+          from: "./node_modules/potree/resources/textures/",
+          to: "libs/potree/resources/textures/"
+        }
       ])
     ]
   }
